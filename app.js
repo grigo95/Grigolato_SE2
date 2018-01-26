a288e3f5b86ef723a87ba1230f974a82ea316d56
Nicholas <nicholas.grigolato@studenti.unitn.it>
2018-01-26 11:31:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express'),
     bodyParser = require('body-parser');
 
+const check = require('./checker.js');
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -12,6 +14,24 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/count',function (req, res) {
     res.json({count: 5})
 })
+
+
+app.post('/check', (req, res) => {
+    var url = req.body.url;
+    var invocationParameters = req.body.invocationParameters;
+    var expectedResultData = req.body.expectedResultData;
+    var expectedResultStatus = req.body.expectedResultStatus;
+
+    var response = {};
+
+    check(url, invocationParameters, expectedResultData, expectedResultStatus).then((response) => {
+        console.log('Response: ');
+        console.log(response);
+
+        res.send(response);
+        res.end();
+    });
+});
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
